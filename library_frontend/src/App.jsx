ef474a8bcdda745973e080ad7ad6790fd5b52c94
Oudone Phengkhamlar <oudonephengkhamlar@gmail.com>
2024-05-05 08:19:06 +0700

    update

---
+++ library_frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
